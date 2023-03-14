cef6ced0e629383da45e0ac54348f5a746d50e7a
patico14 <110640109+patico14@users.noreply.github.com>
2023-03-14 18:23:24 -0500

    using useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+const GIFS = [
+  'https://media1.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif?cid=ecf05e47ywwwpeeyvg1iv6yafwrcy32nnn0uetvctz9o4t5m&rid=giphy.gif&ct=g',
+  'https://media3.giphy.com/media/EPcvhM28ER9XW/giphy.gif?cid=ecf05e471sw64ybhibdsv4ewcenzkjluo3h6dxn5ohbttv3d&rid=giphy.gif&ct=g'
+  
+]
+
+const DIFERENT_GIFS = [
+  'https://media2.giphy.com/media/mCRJDo24UvJMA/200.webp?cid=ecf05e47g8mcucsj4tu1fvygrici09csfv20dyi1fkfgcuvs&rid=200.webp&ct=g'
+]
+
 function App() {
+  const [gifs, setGifts] = useState(GIFS)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-header">
+        {
+          gifs.map(singleGifs => <img src={singleGifs}/>) 
+        }
+        <button onClick={() => setGifts(DIFERENT_GIFS)}>CAMBIAR GIFTS</button>
+       
+      </section>
     </div>
   );
 }
