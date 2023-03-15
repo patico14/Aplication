2e66a921c2ea956e730fd04a09ac341b4177a1ad
patico14 <110640109+patico14@users.noreply.github.com>
2023-03-14 23:07:52 -0500

    Api call

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-const GIFS = [
-  'https://media1.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif?cid=ecf05e47ywwwpeeyvg1iv6yafwrcy32nnn0uetvctz9o4t5m&rid=giphy.gif&ct=g',
-  'https://media3.giphy.com/media/EPcvhM28ER9XW/giphy.gif?cid=ecf05e471sw64ybhibdsv4ewcenzkjluo3h6dxn5ohbttv3d&rid=giphy.gif&ct=g'
-  
-]
-
-const DIFERENT_GIFS = [
-  'https://media2.giphy.com/media/mCRJDo24UvJMA/200.webp?cid=ecf05e47g8mcucsj4tu1fvygrici09csfv20dyi1fkfgcuvs&rid=200.webp&ct=g'
-]
+const apiURL = 'https://api.giphy.com/v1/gifs/search?api_key=xFcdXd43xKNG5f7GoLvvzmFHrttOlYL1&q=pand&limit=25&offset=0&rating=g&lang=en'
 
 function App() {
-  const [gifs, setGifts] = useState(GIFS)
+  const [gifs, setGifts] = useState([])
+
+  useEffect(function(){
+    fetch(apiURL)
+    .then(res => res.json())
+    .then(response => {
+      const {data} = response
+      const gifs = data.map(image => image.images.downsized_medium)
+      setGifts(gifs)
+  })
+    // setGifts(DIFERENT_GIFS)
+  }, [])
   
   return (
     <div className="App">
@@ -21,7 +24,7 @@ function App() {
         {
           gifs.map(singleGifs => <img src={singleGifs}/>) 
         }
-        <button onClick={() => setGifts(DIFERENT_GIFS)}>CAMBIAR GIFTS</button>
+
        
       </section>
     </div>
