2969978604d9fd265d728e57897bc79bda0c61ef
patico14 <110640109+patico14@users.noreply.github.com>
2023-03-16 00:02:28 -0500

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
 
 import ListOfGifs from "./components/ListOfGifs";
 
 function App() {
-  const [keyword, setKeyword] = useState('cat')
+  
 
   return (
     <div className="App">
       <section className="App-header">
-        <button onClick={() => setKeyword('dog')}>cambiar keyword</button>
-      <ListOfGifs Keyword={keyword}/>
+      <ListOfGifs Keyword='panda'/>
       </section>
     </div>
   );
